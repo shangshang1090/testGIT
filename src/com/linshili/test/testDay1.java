6c9dfc7c96c00c7c022f1d3defe888269173dbbf
shangahsng1090 <1090607853@qq.com>
2022-08-15 11:50:31 +0800

    hello git second

--- src/com/linshili/test/testDay1.java
+++ src/com/linshili/test/testDay1.java
@@ -3,5 +3,6 @@ package com.linshili.test;
 public class testDay1 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
     }
 }
