4c7c1ced95a7a9efe228f6c85d26decf6f5c8cd1
shangahsng1090 <1090607853@qq.com>
2022-08-15 12:34:26 +0800

    hello git branch

--- src/com/linshili/test/testDay1.java
+++ src/com/linshili/test/testDay1.java
@@ -4,5 +4,6 @@ public class testDay1 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("hello branch");
     }
 }
